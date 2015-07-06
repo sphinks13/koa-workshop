0795294d31cdd723b8f85d9d8537cfef8c63e97c
sphinks <alex.tatarikov@gmail.com>
2015-07-06 23:47:16 +0300

    03 - done

--- 03-routing/index.js
+++ 03-routing/index.js
@@ -3,14 +3,22 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
+app.use(function* (next) {
+    if(this.request.path != '/') return yield next;
 
+    this.response.body = 'hello world';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+    if(this.request.path != '/404') return yield next;
 
+    this.response.status = 404;
+    this.response.message = 'page not found';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+    if(this.request.path != '/500') return yield next;
 
+    this.response.status = 500;
+    this.response.message = 'internal server error';
 });
