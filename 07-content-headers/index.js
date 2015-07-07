f1c25bf2dbafbdde8de650a2f91bcbf64f9c893c
sphinks <alex.tatarikov@gmail.com>
2015-07-07 19:52:27 +0300

    07 - done

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,9 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
-})
+    if (this.request.is('application/json')) {
+        this.response.body = {message: 'hi!'};
+    } else {
+        this.response.body = 'ok';
+    }
+});
